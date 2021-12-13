c2e0ccf7a849fc15e21c5984831210c28c96e42a
swetha1702 <swetha.1901249@srec.ac.in>
2021-12-13 22:41:54 +0530

    changes made

--- src/components/pages/AchievementPage/Achievements.js
+++ src/components/pages/AchievementPage/Achievements.js
@@ -13,12 +13,13 @@ import { InfoSection } from "../../InfoSection/InfoSection.js";
 function Achievements() {
   return (
     <>
-    <YoutubeEmbed embedId="2PnmLF9YQRI" />
+    
       <InfoSection {...AboutObjOne} />
       <InfoSection {...AboutObjTwo} />
       <InfoSection {...AboutObjThree} />
       <InfoSection {...AboutObjFour} />
       <InfoSection {...AboutObjFive} />
+      <YoutubeEmbed embedId="2PnmLF9YQRI" />
       
     </>
   );
