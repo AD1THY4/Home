bbe9fcd3e3058940a6e3203055a6d5fb2d3eb82a
AD1THY4 <adithyakrishnanpillai@gmail.com>
2021-12-15 19:33:09 +0530

    second commit

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -10,12 +10,46 @@ function App() {
 
   return (
     <div className="whole">
+      <div className="anim1">
+        <b>
+          <i>HELLO WORLD</i>
+        </b>
+      </div>
+
+      <center>
+        <div className="Container">
+          <center>
+            {width > 900 ? (
+              <div className="Dis">
+                <b>Google Develops Students Club </b>
+                <div className="clg">
+                  <b>Sri Ramakrishna Engineering College</b>
+                </div>
+              </div>
+            ) : (
+              <div className="Dis">
+                <b>GDSC </b>
+                <div className="clg">
+                  <b>SREC</b>
+                </div>
+              </div>
+            )}
+          </center>
+        </div>
+      </center>
+      <div className="anim2">
+        <b>
+          <i>{"#INCLUDE<STDIO.H>"}</i>
+        </b>
+      </div>
+      <br />
+      <br />
       <div className="Start">
         <div className="slider">
           {width > 900 ? (
             <CarouselProvider
               naturalSlideWidth={100}
-              naturalSlideHeight={25}
+              naturalSlideHeight={40}
               totalSlides={3}
               isPlaying={true}
               interval={2000}
@@ -28,7 +62,7 @@ function App() {
                     <Image
                       isBgImage="true"
                       className="firstslide"
-                      src="https://i.imgur.com/uStkuTk.png"
+                      src="{data.image}"
                       alt="fs"
                     />
                   </Slide>
@@ -50,12 +84,11 @@ function App() {
                   </Slide>
                 </Slider>
               </div>
-              <br />
             </CarouselProvider>
           ) : (
             <CarouselProvider
               naturalSlideWidth={100}
-              naturalSlideHeight={60}
+              naturalSlideHeight={85}
               totalSlides={3}
               isPlaying={true}
               interval={2000}
@@ -90,75 +123,174 @@ function App() {
                   </Slide>
                 </Slider>
               </div>
-              <br />
             </CarouselProvider>
           )}
         </div>
       </div>
-      <div className="desc1" style={{ display: "table-cell" }}></div>
-      <div className="desc2" style={{ display: "table-cell" }}></div>
+
       <div style={{ display: "table" }} className="design">
         <br />
         <br />
-        <div style={{ display: "table-row" }}>
-          <div style={{ display: "table-cell" }} className="emptyspace"></div>
+        {width > 900 ? (
+          <div style={{ display: "table-row" }}>
+            <div style={{ display: "table-cell" }} className="emptyspace"></div>
 
-          <div style={{ display: "table-cell" }} className="Icon1">
-            <center>
-              <Icon icon="el:group" color="#34a853" width="100" height="100" />
-            </center>
-            <center className="connect">
-              <b>Connect</b>
-            </center>
-            <br />
-            <div className="label1">
-              Meet students interested in developer technologies at your
-              university. All are welcome, including those with diverse
-              backgrounds and different majors.
+            <div style={{ display: "table-cell" }} className="Icon1">
+              <center>
+                {width > 900 ? (
+                  <Icon
+                    icon="el:group"
+                    color="#8a8e97"
+                    width="100"
+                    height="100"
+                  />
+                ) : (
+                  <Icon
+                    icon="el:group"
+                    color="#8a8e97"
+                    width="50"
+                    height="50"
+                  />
+                )}
+              </center>
+              <center className="connect">
+                <b>Connect</b>
+              </center>
+              <br />
+              <div className="label1">
+                Meet students interested in developer technologies at your
+                university. All are welcome, including those with diverse
+                backgrounds and different majors.
+              </div>
             </div>
-          </div>
-          <div style={{ display: "table-cell" }} className="emptyspace"></div>
-          <div style={{ display: "table-cell" }} className="Icon2">
-            <center>
-              <Icon
-                icon="dashicons:welcome-learn-more"
-                color="#34a853"
-                width="100"
-                height="100"
-              />
-            </center>
-            <center>
-              <b>Learn</b>
-            </center>
-            <br />
-            <div className="label2">
-              Learn about a range of technical topics and gain new skills
-              through hands-on workshops, events, talks, and project-building
-              activities - both online and in-person.
+            <div style={{ display: "table-cell" }} className="emptyspace"></div>
+            <div style={{ display: "table-cell" }} className="Icon2">
+              <center>
+                {" "}
+                {width > 900 ? (
+                  <Icon
+                    icon="dashicons:welcome-learn-more"
+                    color="#8a8e97"
+                    width="100"
+                    height="100"
+                  />
+                ) : (
+                  <Icon
+                    icon="dashicons:welcome-learn-more"
+                    color="#8a8e97"
+                    width="50"
+                    height="50"
+                  />
+                )}
+              </center>
+              <center>
+                <b>Learn</b>
+              </center>
+              <br />
+              <div className="label2">
+                Learn about a range of technical topics and gain new skills
+                through hands-on workshops, events, talks, and project-building
+                activities - both online and in-person.
+              </div>
             </div>
-          </div>
-          <div style={{ display: "table-cell" }} className="emptyspace"></div>
-          <div style={{ display: "table-cell" }} className="Icon3">
-            <center>
-              <Icon
-                icon="uil:arrow-growth"
-                color="#34a853"
-                width="100"
-                height="100"
-              />
-            </center>
-            <center>
-              <b>Grow</b>
-            </center>
-            <br />
-            <div className="label3">
-              Learn about a range of technical topics and gain new skills
-              through hands-on workshops, events, talks, and project-building
-              activities - both online and in-person.
+            <div style={{ display: "table-cell" }} className="emptyspace"></div>
+            <div style={{ display: "table-cell" }} className="Icon3">
+              <center>
+                {width > 900 ? (
+                  <Icon
+                    icon="uil:arrow-growth"
+                    color="#8a8e97"
+                    width="100"
+                    height="100"
+                  />
+                ) : (
+                  <Icon
+                    icon="uil:arrow-growth"
+                    color="#8a8e97"
+                    width="50"
+                    height="50"
+                  />
+                )}
+              </center>
+              <center>
+                <b>Grow</b>
+              </center>
+              <br />
+              <div className="label3">
+                Learn about a range of technical topics and gain new skills
+                through hands-on workshops, events, talks, and project-building
+                activities - both online and in-person.
+              </div>
             </div>
+            <div style={{ display: "table-cell" }} className="emptyspace"></div>
           </div>
-          <div style={{ display: "table-cell" }} className="emptyspace"></div>
-        </div>
+        ) : (
+          <div style={{ display: "table-row" }}>
+            <div></div>
+
+            <div className="Icon1" style={{ display: "table-cell" }}>
+              <center>
+                <Icon
+                  icon="el:group"
+                  color="#8a8e97"
+                  width="100"
+                  height="100"
+                />
+              </center>
+              <center>
+                <b>Connect</b>
+              </center>
+              <br />
+              <div className="label1">
+                Meet students interested in developer technologies at your
+                university. All are welcome, including those with diverse
+                backgrounds and different majors.
+              </div>
+            </div>
+            <div></div>
+            <div className="Icon2" style={{ display: "table-cell" }}>
+              <center>
+                {" "}
+                <Icon
+                  icon="dashicons:welcome-learn-more"
+                  color="#8a8e97"
+                  width="100"
+                  height="100"
+                />
+              </center>
+              <center>
+                <b>Learn</b>
+              </center>
+              <br />
+              <div className="label2">
+                Learn about a range of technical topics and gain new skills
+                through hands-on workshops, events, talks, and project-building
+                activities - both online and in-person.
+              </div>
+            </div>
+            <div></div>
+            <div className="Icon3" style={{ display: "table-cell" }}>
+              <center>
+                <Icon
+                  icon="uil:arrow-growth"
+                  color="#8a8e97"
+                  width="100"
+                  height="100"
+                />
+              </center>
+              <center>
+                <b>Grow</b>
+              </center>
+              <br />
+              <div className="label3">
+                Learn about a range of technical topics and gain new skills
+                through hands-on workshops, events, talks, and project-building
+                activities - both online and in-person.
+              </div>
+            </div>
+            <div></div>
+          </div>
+        )}
         <br />
         <br />
       </div>
